760278ff9d55104c4f514d258f5167624ce352fb
olayiwola <olayiwola@compucorp.co.uk>
2021-11-18 14:39:12 +0000

    WHF-289: Add support for participant token to certificate

--- certificate.php
+++ certificate.php
@@ -177,6 +177,7 @@ function certificate_civicrm_permission(&$permissions) {
 function _compucertificate_add_token_subscribers() {
   Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Case());
   Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Event());
+  Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Participant());
 }
 
 function _compucertificate_getCaseIdFromUrlIfExist() {
@@ -196,6 +197,7 @@ function _compucertificate_getCaseIdFromUrlIfExist() {
 function certificate_civicrm_tokens(&$tokens) {
   $tokens[CRM_Certificate_Token_Case::TOKEN] = CRM_Certificate_Token_Case::prefixedEntityTokens();
   $tokens[CRM_Certificate_Token_Event::TOKEN] = CRM_Certificate_Token_Event::prefixedEntityTokens();
+  $tokens[CRM_Certificate_Token_Participant::TOKEN] = CRM_Certificate_Token_Participant::prefixedEntityTokens();
 
   if (_compucertificate_getCaseIdFromUrlIfExist()) {
     $tokens['certificate_url']['certificate_url.case'] = 'Case Certificate URL';
