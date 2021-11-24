d0bf5eb33855dc5e2112723d95788acf69d3658c
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2021-11-24 15:22:52 +0100

    MAE-628: Add support for event certificates to getContactCertificates API endpoint

--- CRM/Certificate/Entity/Event.php
+++ CRM/Certificate/Entity/Event.php
@@ -157,11 +157,27 @@ class CRM_Certificate_Entity_Event implements CRM_Certificate_Entity_EntityInter
     $configuredCertificates = CompuCertificate::getEntityCertificates(CertificateType::EVENTS);
 
     foreach ($configuredCertificates as $configuredCertificate) {
-      $result = civicrm_api3('Participant', 'get', [
-        'event_id' => $configuredCertificate['entity_type_id'],
-        'status_id' => $configuredCertificate['status_id'],
+      $eventAttribute = $this->getCertificateEventAttribute($configuredCertificate['certificate_id']);
+      $participantTypeId = array_column($eventAttribute, 'participant_type_id');
+
+      $condition = [
+        'contact_id' => $contactId,
         'api.Event.get' => ['is_active' => 1],
-      ]);
+      ];
+
+      if (!empty($configuredCertificate['entity_type_id'])) {
+        $condition['event_id'] = $configuredCertificate['entity_type_id'];
+      }
+
+      if (!empty($configuredCertificate['status_id'])) {
+        $condition['participant_status_id'] = $configuredCertificate['status_id'];
+      }
+
+      if (!empty($participantTypeId)) {
+        $condition['participant_role_id'] = ['IN' => (array) $participantTypeId];
+      }
+
+      $result = civicrm_api3('Participant', 'get', $condition);
 
       if ($result['is_error']) {
         continue;
@@ -177,6 +193,7 @@ class CRM_Certificate_Entity_Event implements CRM_Certificate_Entity_EntityInter
           'type' => 'Event',
           'linked_to' => $participant['event_title'],
           'download_link' => $this->getCertificateDownloadUrl($participant['id'], $contactId, TRUE),
+          'participant_role' => $participant['participant_role'],
         ];
         array_push($certificates, $certificate);
       });
