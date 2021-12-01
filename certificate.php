b9bbe816c8fb414767764f9cfffce181801b3af1
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2021-12-01 12:41:59 +0100

    WHF-291: Add support for membership certificate tokens

--- certificate.php
+++ certificate.php
@@ -178,6 +178,7 @@ function _compucertificate_add_token_subscribers() {
   Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Case());
   Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Event());
   Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Participant());
+  Civi::dispatcher()->addSubscriber(new CRM_Certificate_Token_Membership());
 }
 
 function _compucertificate_getCaseIdFromUrlIfExist() {
@@ -198,6 +199,7 @@ function certificate_civicrm_tokens(&$tokens) {
   $tokens[CRM_Certificate_Token_Case::TOKEN] = CRM_Certificate_Token_Case::prefixedEntityTokens();
   $tokens[CRM_Certificate_Token_Event::TOKEN] = CRM_Certificate_Token_Event::prefixedEntityTokens();
   $tokens[CRM_Certificate_Token_Participant::TOKEN] = CRM_Certificate_Token_Participant::prefixedEntityTokens();
+  $tokens[CRM_Certificate_Token_Membership::TOKEN] = CRM_Certificate_Token_Membership::prefixedEntityTokens();
 
   if (_compucertificate_getCaseIdFromUrlIfExist()) {
     $tokens['certificate_url']['certificate_url.case'] = 'Case Certificate URL';
