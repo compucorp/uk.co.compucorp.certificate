59a236c6b8b77e346239ada1548102d440ab077b
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2021-12-20 09:13:29 +0100

    WHF-297: Update create, update and delete notifications according to spec

--- CRM/Certificate/Form/CertificateConfigure.php
+++ CRM/Certificate/Form/CertificateConfigure.php
@@ -145,9 +145,11 @@ class CRM_Certificate_Form_CertificateConfigure extends CRM_Core_Form {
       return;
     }
 
-    $msg = sprintf('Certificate configuration %s successfully', !empty($this->_id) ? 'updated' : 'created');
+    $createOrUpdate = !empty($this->_id) ? 'updated' : 'created';
 
-    CRM_Core_Session::setStatus($msg, 'success', 'success');
+    $msg = sprintf('Certificate configuration %s successfully', $createOrUpdate);
+
+    CRM_Core_Session::setStatus($msg, 'Item ' . $createOrUpdate, 'success');
   }
 
   public function setDefaultValues() {
