7c5356ebac993c7a02bded78ad3821bf6eefd5e9
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-01-06 12:17:57 +0100

    FOSFAB-37: Add default image format option group

--- CRM/Certificate/Hook/PostInstall/AddCertificateImageFormatOptionGroup.php
+++ CRM/Certificate/Hook/PostInstall/AddCertificateImageFormatOptionGroup.php
@@ -1,26 +1,62 @@
 <?php
 
+use CRM_Certificate_BAO_CompuCertificateImageFormat as CompuCertificateImageFormat;
+
 class CRM_Certificate_Hook_PostInstall_AddCertificateImageFormatOptionGroup {
+
+  /**
+   * Reflects if the option group was just created.
+   *
+   * @var bool
+   */
+  private $fresh = FALSE;
 
   public function apply(): bool {
     $this->createImageFormatOptionGroup();
+    $this->addDefaultImageFormats();
 
     return TRUE;
   }
 
   public function createImageFormatOptionGroup(): void {
     $count = civicrm_api3('OptionGroup', 'getcount', [
-      'name' => CRM_Certificate_BAO_CompuCertificateImageFormat::NAME,
+      'name' => CompuCertificateImageFormat::NAME,
     ]);
 
     if ((int) $count == 0) {
+      $this->fresh = TRUE;
       civicrm_api3('OptionGroup', 'create', [
-        'name' => CRM_Certificate_BAO_CompuCertificateImageFormat::NAME,
+        'name' => CompuCertificateImageFormat::NAME,
         'title' => 'Certificate Image Formats',
         'is_reserved' => 1,
         'is_active' => 1,
       ]);
     }
+  }
+
+  public function addDefaultImageFormats(): void {
+    if (!$this->fresh) {
+      return;
+    }
+
+    $imageFormatBao = new CompuCertificateImageFormat();
+    $jpgFormat = [
+      'name' => 'JPG',
+      'description' => 'JPG Image Format',
+      'quality' => 10,
+      'extension' => 'jpg',
+      'is_default' => TRUE,
+    ];
+    $pngFormat = [
+      'name' => 'PNG',
+      'description' => 'PNG Image Format',
+      'quality' => 10,
+      'extension' => 'png',
+      'is_default' => FALSE,
+    ];
+
+    $imageFormatBao->saveImageFormat($jpgFormat);
+    $imageFormatBao->saveImageFormat($pngFormat);
   }
 
 }
