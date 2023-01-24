9febd2662a74593f231aa9066d6a83e6d432cf6f
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-01-24 14:03:49 +0100

    FOSFAB-44: Add Endpoint to retrieve contact certificates by relationshiptypes

--- CRM/Certificate/Api/Wrapper/CompuCertificate.php
+++ CRM/Certificate/Api/Wrapper/CompuCertificate.php
@@ -10,6 +10,17 @@ use CRM_Certificate_Enum_CertificateType as CertificateType;
 class CRM_Certificate_Api_Wrapper_CompuCertificate implements API_Wrapper {
 
   /**
+   * Entities supported by the API.
+   *
+   * @var array
+   */
+  public const SUPPORTED_ENTITY_PARAMS = [
+    'case' => CertificateType::CASES,
+    'event' => CertificateType::EVENTS,
+    'membership' => CertificateType::MEMBERSHIPS,
+  ];
+
+  /**
    * Returns list of certificates accessible to loggedin user.
    *
    * @param array $params
@@ -19,46 +30,51 @@ class CRM_Certificate_Api_Wrapper_CompuCertificate implements API_Wrapper {
    *   Result with the list of certificates.
    */
   public function getContactCertificates(array $params) {
-    $loggedContactID = CRM_Core_Session::singleton()->getLoggedInContactID();
-    // If no contact_id is provided we default to the logged in contact ID.
-    $contactId = $params['contact_id'] ?? $loggedContactID;
+    $params = $this->setDefaultParams($params);
 
-    $certificates = [];
-
-    if (empty($contactId)) {
+    if (empty($params['contact_id'])) {
       return [];
     }
 
-    switch (TRUE) {
-      case empty($params['entity']):
-        $certificates = $this->getContactEntityCertificate(CertificateType::CASES, $contactId);
-        break;
-
-      case $params['entity'] === 'case':
-        $certificates = $this->getContactEntityCertificate(CertificateType::CASES, $contactId);
-        break;
-
-      case $params['entity'] === 'event':
-        $certificates = $this->getContactEntityCertificate(CertificateType::EVENTS, $contactId);
-        break;
-
-      case $params['entity'] === 'membership':
-        $certificates = $this->getContactEntityCertificate(CertificateType::MEMBERSHIPS, $contactId);
-        break;
-
-      default:
-        $certificates = [];
-        break;
+    $entity = CRM_Utils_Array::value($params['entity'], self::SUPPORTED_ENTITY_PARAMS);
+    if (!empty($entity)) {
+      $entity = CRM_Certificate_Entity_EntityFactory::create($entity);
+      return $entity->getContactCertificates($params['contact_id']);
     }
 
-    return $certificates;
+    return [];
   }
 
-  private function getContactEntityCertificate(int $entity, int $contactId) {
-    $entity = CRM_Certificate_Entity_EntityFactory::create($entity);
-    $certificates = $entity->getContactCertificates($contactId);
+  /**
+   * Returns list of certificates contact has access to based on relationship.
+   *
+   * @param array $params
+   *   Parameters array for the API call.
+   *
+   * @return array
+   *   Result with the list of certificates.
+   */
+  public function getRelationshipCertificates(array $params) {
+    $params = $this->setDefaultParams($params);
 
-    return $certificates;
+    if (empty($params['contact_id'])) {
+      return [];
+    }
+
+    $entity = CRM_Utils_Array::value($params['entity'], self::SUPPORTED_ENTITY_PARAMS);
+    if (!empty($entity)) {
+      $entity = CRM_Certificate_Entity_EntityFactory::create($entity);
+      return $entity->getRelationshipCertificates($params['contact_id']);
+    }
+
+    return [];
+  }
+
+  private function setDefaultParams($params) {
+    return array_merge([
+      'entity' => 'case',
+      'contact_id' => CRM_Core_Session::singleton()->getLoggedInContactID(),
+    ], $params);
   }
 
   /**
