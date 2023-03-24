a0fce7690df87195ff46b4c59e3208d4b71b5c98
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-03-24 11:28:35 +0100

    FOSFAB-102: Add test to ensure contact token is resolved

--- CRM/Certificate/Test/Fabricator/Contact.php
+++ CRM/Certificate/Test/Fabricator/Contact.php
@@ -1,5 +1,7 @@
 <?php
 
+use Civi\Api4\Address;
+
 /**
  * Fabricates contacts using API calls.
  */
@@ -21,6 +23,41 @@ class CRM_Certificate_Test_Fabricator_Contact {
       $params
     );
     return array_shift($result['values']);
+  }
+
+  public static function fabricateWithAddress($params = []) {
+    $params = array_merge(self::$defaultParams, $params);
+    $params['display_name'] = "{$params['first_name']} {$params['last_name']}";
+    $result = civicrm_api3(
+      'Contact',
+      'create',
+      $params
+    );
+    $contact = array_shift($result['values']);
+
+    $contact['address'] = Address::save(FALSE)
+      ->addRecord([
+        "contact_id" => $contact['id'],
+        "location_type_id" => 5,
+        "is_primary" => TRUE,
+        "is_billing" => TRUE,
+        "street_address" => "Coldharbour Ln",
+        "street_number" => "42",
+        "supplemental_address_1" => "Supplementary Address 1",
+        "supplemental_address_2" => "Supplementary Address 2",
+        "supplemental_address_3" => "Supplementary Address 3",
+        "city" => "Hayes",
+        "postal_code" => "UB3 3EA",
+        "country_id" => 1226,
+        "manual_geo_code" => FALSE,
+        "timezone" => NULL,
+        "name" => NULL,
+        "master_id" => NULL,
+      ])
+      ->execute()
+      ->first();
+
+    return $contact;
   }
 
 }
---
+++ tests/phpunit/CRM/Certificate/Token/ContactTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Civi\Api4\Country;
+use CRM_Certificate_Test_Fabricator_Contact as ContactFabricator;
+
+/**
+ * Test case token fields are resolved
+ *
+ * @group headless
+ */
+class CRM_Certificate_Token_ContactTest extends BaseHeadlessTest {
+
+  use CRM_Certificate_Test_Helper_Case;
+
+  public function testCanResolveContactEmployerAddressTokenFields() {
+    $contactTokenSubscriber = new CRM_Certificate_Token_Contact([]);
+
+    $employer = ContactFabricator::fabricateWithAddress();
+    $contact = ContactFabricator::fabricate(['employer_id' => $employer['id']]);
+
+    $tokenValueEventMock = $this->createMock(\Civi\Token\Event\TokenValueEvent::class);
+    $tokenProcessorMock = $this->createMock(\Civi\Token\TokenProcessor::class);
+
+    $tokenValueEventMock->method('getTokenProcessor')->willReturn($tokenProcessorMock);
+    $tokenProcessorMock->method('getContextValues')->willReturn([$contact['id']]);
+
+    $country = Country::get(FALSE)->addWhere('id', '=', $employer['address']['country_id'])->execute()->first();
+    $res = $contactTokenSubscriber->prefetch($tokenValueEventMock);
+
+    $this->assertArrayHasKey("employer_inline_address", $res);
+    $address = explode(", ", $res["employer_inline_address"]);
+
+    // Ensure the order is as expected.
+    $this->assertEquals($address[0], $employer["address"]["supplemental_address_1"]);
+    $this->assertEquals($address[1], $employer["address"]["supplemental_address_2"]);
+    $this->assertEquals($address[2], $employer["address"]["city"]);
+    $this->assertEquals($address[3], $country["name"]);
+  }
+
+}
