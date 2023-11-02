23e32fb517098a495cfe7d235ad6781fd6d6f87e
shahrukh-moby <shahrukh.naeem@edu2x.com>
2023-11-02 11:46:34 +0500

    MMB-270: Add min and max dates to test fabricators

--- CRM/Certificate/Test/Fabricator/Event.php
+++ CRM/Certificate/Test/Fabricator/Event.php
@@ -17,6 +17,8 @@ class CRM_Certificate_Test_Fabricator_Event {
     return [
       'title' => md5(mt_rand()),
       'start_date' => date('Y-m-d'),
+      'min_valid_from_date' => date("Y-m-d"),
+      'max_valid_through_date' => date("Y-m-d", strtotime(date("Y-m-d") . " + 30 days")),
       'event_type_id' => 1,
       'summary' => md5(mt_rand()),
       'description' => md5(mt_rand()),
