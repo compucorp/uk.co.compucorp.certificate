4fb905330365e38306c29ec51988e06a27a27297
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-11-14 16:46:30 +0100

    MMB-282: Allow user to specify certificate download file

---
+++ CRM/Certificate/Enum/DownloadType.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Declares the constants for supported download formats.
+ *
+ * This is different from DownloadFormat, as this specifies
+ * the content that will be returned to the user.
+ */
+class CRM_Certificate_Enum_DownloadType {
+
+  const TEMPLATE = '1';
+  const FILE_DOWNLOAD = '2';
+
+}
