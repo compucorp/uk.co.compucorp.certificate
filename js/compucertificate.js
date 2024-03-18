d1ebdac08c4afab20aead1d3b8effe2b5bc4016a
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2024-03-18 13:05:39 +0100

    FOSFA-282: Support configuration of similar Membership certificate

--- js/compucertificate.js
+++ js/compucertificate.js
@@ -15,8 +15,15 @@ window.downloadLink = function () {
     `<span class="ui-button-icon ui-icon crm-i fa-print"></span>
     <span class="ui-button-icon-space">Print Certificate</span>`
   );
+
+  let url = '';
+  let count = 0;
+  CRM.vars.certificate.download_url.forEach(downloadUrl => {
+    url += ' setTimeout(function(){ window.open("' + downloadUrl + '"); }, ' + (500 * count++) + '); ';
+  });
+
   btn.setAttribute('type', 'button');
-  btn.setAttribute('onclick', "window.open('" + CRM.vars.certificate.download_url + "')");
+  btn.setAttribute('onclick', url);
   btn.setAttribute('target', '_blank');
   btn.classList.add('ui-button', 'ui-corner-all', 'ui-widget');
   btn.style.marginRight = '5px';
